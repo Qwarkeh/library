65a954ee488366b4864bf56dbaf8649e825c029c
Qwarkeh <christine.f.stark@gmail.com>
2022-03-10 11:15:28 -0500

    Adds read status and console.log loop for myLibrary array

--- script.js
+++ script.js
@@ -1,20 +1,40 @@
-// let myLibrary = [];
+let myLibrary = [];
 
-// function Book (title, author, pages) {
-//     this.title = title;
-//     this.author = author;
-//     this.pages = pages;
-// }
+function Book (title, author, pages, readStatus) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.readStatus= readStatus;
+}
 
-// function addBookToLibrary() {
-// }
-
-const btn = document.querySelector('#btnSubmit');
-btn.addEventListener('click', () => {
-
+function addBookToLibrary() {
+    // obtain user input from form
     const titleInput = document.querySelector('#titleInput').value;
     const authorInput = document.querySelector('#authorInput').value;
     const pagesInput = document.querySelector('#pagesInput').value;
+    const readStatusInput = document.querySelector('input[name=readStatus]:checked').value;
 
-    console.log(`${titleInput}, ${authorInput}, ${pagesInput}`);
+    //create new book object with form information
+    const newBook = new Book(titleInput, authorInput, pagesInput, readStatusInput);
+    console.table(newBook);
+
+    //feed the book object into array
+    myLibrary.push(newBook);
+}
+
+function removeLibraryDisplay() {
+
+}
+
+function displayLibrary(myLibrary){
+    for (i = 0; i < myLibrary.length; i++) {
+        //console.log(`${myLibrary[i].title}, ${myLibrary[i].author}, ${myLibrary[i].pages}, ${myLibrary[i].readStatus}`);
+
+        //add to table
+    }
+}
+
+const btn = document.querySelector('#btnSubmit');
+btn.addEventListener('click', () => {
+    addBookToLibrary();
 });
